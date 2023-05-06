53d16b274d9f21da61679dc1514696d4c64d62da
Nhan Le <lenhanasus4@gmail.com>
2023-05-06 17:03:05 +0700

    #intial client project

---
+++ src/components/Layout/Layout.tsx
@@ -0,0 +1,14 @@
+
+import Header from '../Header/Hearder';
+import Footer from '../Footer/Footer';
+
+const Layout = () => {
+  return (
+    <>
+      <Header></Header>
+      <Footer></Footer>
+    </>
+  )
+}
+
+export default Layout;
