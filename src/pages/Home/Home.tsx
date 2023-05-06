43ad3030cbee628857f634dffd07f06de7700c80
Nhan Le <lenhanasus4@gmail.com>
2023-05-06 20:56:12 +0700

    #styling hero section

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,8 +1,34 @@
-import React from 'react'
+import { Col, Container, Row } from 'reactstrap';
+import './home.scss';
+import { Link } from 'react-router-dom';
+import heroImg from '../../assets/images/hero-img.png';
 
 const Home = () => {
+  const year = new Date().getFullYear();
   return (
-    <div>Home</div>
+    <div className='home'>
+      <div className="hero-section">
+        <Container>
+          <Row>
+            <Col>
+              <div className="left">
+                <p>Trending product in {year}</p>
+                <h1>Make your interior more minimalistic & modern</h1>
+                <p>
+                  Lorem ipsum dolor sit, amet consectetur adipisicing elit. Atque distinctio asperiores a id possimus nemo tempore, nam odit quo iusto!
+                </p>
+                <Link to={'/shop'}>
+                  <a href=''>Shop now</a>
+                </Link>
+              </div>
+              <div className="right">
+                <img src={heroImg} alt="" />
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    </div>
   )
 }
 
